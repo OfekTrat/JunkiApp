c8a9e52c0b6dbb9ff80f445dd5940b43d1efa701
Ofek <ofekikotrat@gmail.com>
2021-09-26 20:57:16 +0300

    Cleaned App and Map Activity

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { Component } from 'react';
-import MapActivity from './src/map_activity';
+import MapActivity from './src/map_activity/map_activity';
 import CameraActivity from './src/camera_activity';
 import UploadScreen from './src/upload_screen';
 import ViewImageActivity from './src/view_image';
+import NavigationScreens from './src/navigation_screens';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,19 +14,16 @@ const Stack = createNativeStackNavigator();
 export default class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            screen: 'map'
-        }
     }
 
     render() {
         return (
             <NavigationContainer>
-                <Stack.Navigator initialRouteName="map">
-                    <Stack.Screen name="map" component={MapActivity}/>
-                    <Stack.Screen name="camera" component={CameraActivity}/>
-                    <Stack.Screen name="upload_finding" component={UploadScreen}/>
-                    <Stack.Screen name="view_image" component={ViewImageActivity}/>
+                <Stack.Navigator initialRouteName={NavigationScreens.MAP}>
+                    <Stack.Screen name={NavigationScreens.MAP} component={MapActivity}/>
+                    <Stack.Screen name={NavigationScreens.CAMERA} component={CameraActivity}/>
+                    <Stack.Screen name={NavigationScreens.UPLOAD_FINDING} component={UploadScreen}/>
+                    <Stack.Screen name={NavigationScreens.VIEW_IMAGE} component={ViewImageActivity}/>
                 </Stack.Navigator>
             </NavigationContainer>
         );
---
+++ src/map_activity/map_activity.js
@@ -0,0 +1,103 @@
+import GetFindingByRadius from '../api_communicators/finding_by_radius';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import React, { Component } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { style_map } from './map_styles';
+
+
+
+const defaultRegion = {
+    latitude: 31.7683,
+    longitude: 35.2137,
+    latitudeDelta: 0.015,
+    longitudeDelta: 0.0121
+};
+
+export default class MapActivity extends Component {
+    DEGREES_TO_KM = 111;
+    MAXIMUM_RADIUS = 100;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            style: style_map.map,
+            visible_markers: <View/>
+        };
+
+        this.onTouchablePress = this.navigateToCamera.bind(this);
+        this.initialRegion = this.getDefaultRegion()
+    }
+
+    getDefaultRegion() {
+        return defaultRegion;
+    }
+
+    onRegionChangeComplete = (region) => {
+        const payload = this.generatePayloadByRegion(region);
+
+        if (payload.radius <= this.MAXIMUM_RADIUS) {
+            GetFindingByRadius.get_results(this.setVisibleMarkers, payload);
+        } else {
+            this.setState({visible_markers: <View/>})
+        }
+        
+        
+    }
+    generatePayloadByRegion = (region) => {
+        return {
+            longitude: region.longitude,
+            latitude: region.latitude,
+            radius: this.calcRadius(region.longitudeDelta, region.latitudeDelta)
+        };
+    }
+    calcRadius(longitudeDelta, latitudeDelta) {
+        return Math.ceil(Math.max(longitudeDelta, latitudeDelta) * this.DEGREES_TO_KM);
+    }
+    setVisibleMarkers = (json_data) => {
+        const visible_markers = <View>
+            {
+                json_data &&
+                json_data.map((finding, index) => (
+                    this.renderMarker(index, finding)
+                ))
+            }
+        </View>
+        this.setState({ visible_markers });
+    }
+    renderMarker(key, finding) {
+        return (
+            <Marker key={key} coordinate={{longitude: finding.longitude, latitude: finding.latitude}}/>
+        );
+    }
+
+    navigateToCamera = () => {
+        this.props.navigation.navigate('camera');
+    }
+
+    render() {
+        return (
+            <View
+                style={{flex: 1}}>
+                <MapView
+                    provider={PROVIDER_GOOGLE}
+                    style={style_map.map}
+                    initialRegion={this.initialRegion}
+                    onRegionChangeComplete={this.onRegionChangeComplete}
+                    >   
+                    {this.state.visible_markers}
+                </MapView>
+                <View
+                    style={style_map.buttonView} >
+                     <TouchableOpacity
+                        style={style_map.button}
+                        onPress={this.navigateToCamera} 
+                        >
+                        <Text style={style_map.buttonText}>+</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
+}
+
